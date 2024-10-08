3496e3df261590f46ea5e6811bd1c6491404b7d7
BayInl <dry330500@gmail.com>
2024-10-08 17:27:03 +0800

    todo algorithm

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +51,23 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0{
+            return Err(ParsePersonError::Empty);
+        }
+        let eles = s.split(',').collect::<Vec<&str>>();
+        let (name, age) = match &eles[..]{
+            [name, age] => (name.to_string(), age.parse().map_err(ParsePersonError::ParseInt)?),
+            _ => return Err(ParsePersonError::BadLen),
+        };
+        if name.is_empty(){
+            Err(ParsePersonError::NoName)
+        }
+        else{
+            Ok(Person{
+                name,
+                age,
+            })
+        }
     }
 }
 
